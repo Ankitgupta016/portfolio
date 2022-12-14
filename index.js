ed49cbf5edf58adf0e3d0426ccfb5832028daa5c
Ankitgupta016 <103989854+Ankitgupta016@users.noreply.github.com>
2022-12-14 10:09:03 +0530

    publish

---
+++ index.js
@@ -0,0 +1,15 @@
+// const btnn = document.querySelector(".btnn");
+// const btnns = document.querySelector(".btnns");
+
+// btnn.addEventListener('click', ()=>{
+//     document.body.style.background = "green";
+    
+//     document.body.style.color = "white";
+// })
+
+// btnns.addEventListener('click', ()=>{
+//     document.body.style.background = "white";
+  
+//     document.body.style.color = "white";
+// })
+
